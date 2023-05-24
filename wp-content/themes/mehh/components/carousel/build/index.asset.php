e151f9b3f4287e275c1656ec437989e3748c1615
Madis Luik <madis.luik@eklips.se>
2023-05-24 11:51:21 +0300

    carousel build

--- wp-content/themes/mehh/components/carousel/build/index.asset.php
+++ wp-content/themes/mehh/components/carousel/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c9cf772bd534f7a80921');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'aed0f401d793a524afb9');
