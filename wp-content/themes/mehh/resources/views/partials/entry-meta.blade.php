9b97f4d650a9b26d6523b52b6aa4d610d7c7ffac
Madis Luik <madis.luik@eklips.se>
2023-06-03 11:41:42 +0300

    meta

--- wp-content/themes/mehh/resources/views/partials/entry-meta.blade.php
+++ wp-content/themes/mehh/resources/views/partials/entry-meta.blade.php
@@ -1,15 +1,7 @@
-<div class="entry-meta-container mb-3">
-  <div class="d-flex gap-2">
-    <time class="updated" datetime="{{ get_post_time('c', true) }}">
-      {{ get_the_date() }}
-    </time>
-
-    <p class="byline author vcard mb-0 d-none">
-      <a class="link-primary" href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
-        {{ get_the_author() }}
-      </a>
-    </p>
-  </div>
+<div class="entry-meta-container mb-3 d-flex gap-2">
+  <time class="updated" datetime="{{ get_post_time('c', true) }}">
+    {{ get_the_date() }}
+  </time>
 
   @if($categories)
     <div class="category-list">
