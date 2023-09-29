414436e431073e96eae5f16fe612661990b7c0d5
Madis Luik <madis.luik@eklips.se>
2023-09-29 16:19:22 +0300

    cookie banner

--- wp-content/themes/mehh/resources/views/components/cookie-banner.blade.php
+++ wp-content/themes/mehh/resources/views/components/cookie-banner.blade.php
@@ -1,4 +1,4 @@
 <div id="cookie-banner">
     <p>{{__('This site uses cookies to improve your experience. By continuing to use this site, you consent to our use of cookies.', 'sage')}}</p>
-    <button id="accept-cookies">{{__('Accept', 'sage')}}</button>
+    <button class="btn btn-primary" id="accept-cookies">{{__('Accept', 'sage')}}</button>
 </div>
